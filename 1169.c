62db3c8a9866e16bbf2cb5318ce3eaae08606751
Vinícius Alves Schautz <68827401+schautsu@users.noreply.github.com>
2022-10-11 23:45:07 -0400

    GRAINS IN A CHESS BOARD

---
+++ 1169.c
@@ -0,0 +1,23 @@
+/*1169: GRAINS IN A CHESS BOARD - schautsu*/
+#include <stdio.h>
+#include <math.h>
+#include <inttypes.h>
+
+int main()
+{
+    uint64_t kg;
+    int n, sq;
+
+    scanf("%d", &n);
+    while(n){
+        scanf("%d", &sq);
+
+        kg = (pow(2,sq) - 1) / 12000; 
+        /*(pow(2,sq) - 1): grams
+        12000 grams: 1 kg*/
+        printf("%"PRIu64" kg\n", kg);
+        
+        n--;
+    }
+    
+}
