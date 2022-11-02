9511f094dca80944cc2fed8059bc8096c89e670e
Vinicius Alves Schautz <68827401+schautsu@users.noreply.github.com>
2022-11-02 11:27:47 -0400

    SUB-PRIME

--- 1105.c
+++ 1105.c
@@ -3,30 +3,34 @@
 
 int main()
 {
-    int b, n; /*b: banks | n: debentures*/
+    int b, n; //b: banks | n: debentures
     int i, debtor, creditor, deb, sn;
     
-    while(scanf("%d %d", &b, &n) && (b && n)){
+    while (scanf("%d %d", &b, &n) && (b && n))
+    {
         int money_res[b];
         sn = 0;
 
-        for(i=0;i<b;i++) scanf("%d", &money_res[i]);
-
-        while(n){
+        for(i = 0; i < b; i++)
+            scanf("%d", &money_res[i]);
+        
+        while (n)
+        {
             scanf("%d %d %d", &debtor, &creditor, &deb);
 
             money_res[debtor-1] -= deb;
             money_res[creditor-1] += deb;
+            
             n--;
         }
-
-        for(i=0;i<b;i++){
-            if(money_res[i] < 0)
-            {
+        for ( i = 0; i < b; i++)
+        {
+            if (money_res[i] < 0) {
                 sn++; break;
             }
         }
         if(sn) printf("N\n");
         else printf("S\n");
     }
+    return (0);
 }
