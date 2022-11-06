2bc338e5417d1a1ad9b613b47be30b8e8c340e9a
Vinicius Alves Schautz <68827401+schautsu@users.noreply.github.com>
2022-11-05 21:45:26 -0400

    SOMA DE FATORIAIS

---
+++ 1161.c
@@ -0,0 +1,36 @@
+/*1161: SOMA DE FATORIAIS - schautsu*/
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+/*Function Prototype*/
+int64_t fatorial(int x);
+
+int main()
+{
+    int64_t sum;
+    int m, n;
+
+    while (scanf("%d %d", &m, &n) != EOF)
+    {
+        sum = fatorial(m) + fatorial(n);
+
+        printf("%" SCNd64, sum);
+        printf("\n");
+    }
+    return (0);
+}
+
+int64_t fatorial(int x)
+{
+    int64_t fat = 1;
+    int i;
+
+    if (x >= 0) {
+        for (i = 2; i <= x; i++) {
+            fat *= i;
+        }
+        return (fat);
+    }
+    return (0);
+}
